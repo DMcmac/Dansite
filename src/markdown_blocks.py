053751f3cab6cfab775b624c68d9ad2c6cb6d931
DMcmac <danielmcquitty7@gmail.com>
2026-09-04 15:35:37 -0400

    Added markdown block handling and tests

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -32,4 +32,18 @@ def block_to_block_type(block):
             if not l.startswith('>'):
                 return BlockType.PARAGRAPH
         return BlockType.QUOTE
+    if block.startswith('- '):
+        for l in lines:
+            if not l.startswith('- '):
+                return BlockType.PARAGRAPH
+        return BlockType.UNORDERED_LIST
+    list_order = 1
+    if block.startswith(f'{list_order}. '):
+        for l in lines:
+            if not l.startswith(f'{list_order}. '):
+                return BlockType.PARAGRAPH
+            elif l.startswith(f'{list_order}. '):
+                list_order += 1
+        return BlockType.ORDERED_LIST   
+        
     return BlockType.PARAGRAPH
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,5 +1,5 @@
 import unittest
-from markdown_blocks import markdown_to_blocks
+from markdown_blocks import markdown_to_blocks, block_to_block_type, BlockType
 
 
 class TestMarkdownBlocks(unittest.TestCase):
@@ -43,4 +43,89 @@ This is the same paragraph on a new line
                 "This is another paragraph with _italic_ text and `code` here,\nThis is the same paragraph on a new line",
                 "- This is a list\n- with items",
             ],
+        )
+
+    def test_block_heading(self):
+        md = "### This is a heading block"
+        block_type = block_to_block_type(md)
+        self.assertEqual(
+            block_type, BlockType.HEADING
+            
+        )
+
+    def test_almost_heading(self):
+        md = "##Almost a heading"
+        block_type = block_to_block_type(md)
+        self.assertEqual(
+            block_type, BlockType.PARAGRAPH
+        )
+
+    def test_multiline_block(self):
+        md = "```\nThis is a multiline code block```"
+        block_type = block_to_block_type(md)
+        self.assertEqual(
+            block_type, BlockType.CODE
+        )
+
+    def test_almost_code(self):
+        md = "``\nNot quite a code block```"
+        block_type = block_to_block_type(md)
+        self.assertEqual(
+            block_type, BlockType.PARAGRAPH
+        )
+
+    def test_still_not_code(self):
+        md = "```\nStill not code``"
+        block_type = block_to_block_type(md)
+        self.assertEqual(
+            block_type, BlockType.PARAGRAPH
+        )
+
+    def test_quote_block(self):
+        md = ">This is quote block\n> I am testing multiple lines"
+        block_type = block_to_block_type(md)
+        self.assertEqual(
+            block_type, BlockType.QUOTE
+        )
+
+    def test_not_quote(self):
+        md = ">This is quote block\n I am testing multiple lines"
+        block_type = block_to_block_type(md)
+        self.assertEqual(
+            block_type, BlockType.PARAGRAPH
+        )
+
+    def test_unorderd_list(self):
+        md = "- This is an unordered list\n- With one line after the other"
+        block_type = block_to_block_type(md)
+        self.assertEqual(
+            block_type, BlockType.UNORDERED_LIST
+        )
+
+    def test_not_unorder_list(self):
+        md = "- Almost an unordered list\n But not quite"
+        block_type = block_to_block_type(md)
+        self.assertEqual(
+            block_type, BlockType.PARAGRAPH
+        )
+
+    def test_ordered_list(self):
+        md = "1. This\n2. is\n3. an\n4. ordered\n5. list"
+        block_type = block_to_block_type(md)
+        self.assertEqual(
+            block_type, BlockType.ORDERED_LIST
+        )
+
+    def test_almost_ordered(self):
+        md = "1. This\n3. is\n4. unordered"
+        block_type = block_to_block_type(md)
+        self.assertEqual(
+            block_type, BlockType.PARAGRAPH
+        )
+
+    def test_also_almost_ordered(self):
+        md = "1. This\n2.is\n3. close"
+        block_type = block_to_block_type(md)
+        self.assertEqual(
+            block_type, BlockType.PARAGRAPH
         )
